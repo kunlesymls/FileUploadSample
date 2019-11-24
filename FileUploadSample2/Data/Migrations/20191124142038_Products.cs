b4d28bf88408979ecef3144c895529f81dc91f4d
Joseph Ajileye <kunlesymls@gmail.com>
2019-11-24 15:44:55 +0100

    Add project files.

---
+++ FileUploadSample2/Data/Migrations/20191124142038_Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FileUploadSample2.Data.Migrations
+{
+    public partial class Products : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductCategoryId = table.Column<int>(nullable: false),
+                    ProductName = table.Column<string>(nullable: true),
+                    Alias = table.Column<string>(nullable: true),
+                    ProductPicture = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ FileUploadSample2/Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+
+namespace FileUploadSample2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int ProductCategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string Alias { get; set; }
+        public byte[] ProductPicture { get; set; }
+
+        [NotMapped]
+        public string ImageContentType { get; set; } = "image/png";
+
+        public string GetInLineImgSource()
+        {
+            if (ProductPicture == null || ImageContentType == null)
+            {
+                return null;
+            }
+            var base64Image = Convert.ToBase64String(ProductPicture);
+            return $"data:{ImageContentType};base64,{base64Image}";
+        }
+
+        [Display(Name = "Upload Product Picture")]
+        [NotMapped]
+        public IFormFile File
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+                try
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        value.CopyTo(memoryStream);
+                        ProductPicture = memoryStream.ToArray();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
